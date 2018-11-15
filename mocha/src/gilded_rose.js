a6571b7209cea149917d75428c55c36dfc691a03
Hedi Van Meurs <alisoun@gmail.com>
2018-11-15 14:54:35 +0000

    removed conjured function

--- mocha/src/gilded_rose.js
+++ mocha/src/gilded_rose.js
@@ -51,14 +51,6 @@ class Shop {
     }
   }
 
-function conjured(item) {
-  if (item.sellIn <= 0) {
-    item.quality -= 4;
-  } else {
-    item.quality -= 2;
-  }
-}
-
   function normalItem(item) {
     if (item.sellIn <= 0 || item.name.includes('conjured')) {
       item.quality -= 2;
